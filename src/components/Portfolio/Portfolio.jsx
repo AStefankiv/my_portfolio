94a42d1aa010124d57314fa99be540f9bf1de63e
AStefankiv <stefankif35@gmail.com>
2024-11-23 16:56:30 -0700

    Edit grammar gpt link on main page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -19,7 +19,6 @@ const Portfolio = () => {
         <motion.div variants={textVariant(.4)} className={`flexCenter ${css.heading}`}>
           <div>
             <span className="primaryText">My Latest Works</span>
-            <p style={{marginTop: "10px"}}>Stack: api / css / express.js / html / next.js / node / ruby / react.js / sql / postgres etc...</p>
           </div>
           <a href="https://github.com/AStefankiv" target="_blank" rel="noopener noreferrer" className="secondaryText">More on github</a>
         </motion.div>
@@ -29,9 +28,14 @@ const Portfolio = () => {
         <a href="https://github.com/AStefankiv/jungle" target="_blank" rel="noopener noreferrer">
             <motion.img variants={fadeIn("up", "tween", 0.5, 0.6)} src="./showCase1.gif" alt="project" style={{ width: "350px", height: "auto", borderRadius: "10px" }} />
           </a>
-          <a href="https://gptgrammar.netlify.app/" target="_blank" rel="noopener noreferrer">
-            <motion.img variants={fadeIn("up", "tween", 0.7, 0.6)} src="./showCase2.gif" alt="project" style={{ width: "350px", height: "auto", borderRadius: "10px" }} />
-          </a>
+          <div>
+            <a href="https://gptgrammar.netlify.app/" target="_blank" rel="noopener noreferrer">
+              <motion.img variants={fadeIn("up", "tween", 0.7, 0.6)} src="./showCase2.gif" alt="project" style={{ width: "350px", height: "auto", borderRadius: "10px" }} />
+            </a>
+            <div className="flexCenter">
+            <a href="/grammar-check" className="center-text">more about it</a>
+            </div>
+          </div>
           <a href="https://github.com/AStefankiv/tweeter" target="_blank" rel="noopener noreferrer">
             <motion.img variants={fadeIn("up", "tween", 0.9, 0.6)} src="./showCase3.gif" alt="project" style={{ width: "350px", height: "auto", borderRadius: "10px" }} />
           </a>
--- src/pages/GrammarCheckPage/SimpleSlider.jsx
+++ src/pages/GrammarCheckPage/SimpleSlider.jsx
@@ -7,7 +7,7 @@ const SimpleSlider = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
   };
 
